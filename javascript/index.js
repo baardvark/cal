f5f8646b85dd3752b8d0edc3abacc17bc7fc466e
Spencer Richards <spencer.richards7@gmail.com>
2019-03-04 16:20:31 -0700

    almost finished with template

---
+++ javascript/index.js
@@ -0,0 +1,28 @@
+// Array of items to use
+var items = ["T-shirt", "Necklace", "Shoes", "Backpack", "Hat", "Watch", "Purse", "Belt"];
+// Empty array that will contain all cart items
+var cart = [];
+
+var cartContainer = document.getElementById("cart");
+var itemButtons = document.getElementsByClassName("item-button");
+var showCartButton = document.getElementById("show-cart-button");
+
+for(var i = 0; i < itemButtons.length; i++) {
+  itemButtons[i].addEventListener("click", function () {    
+    cart.push(this.parentElement.children[0].innerText);
+  })
+}
+
+showCartButton.addEventListener("click", function() {
+  // This removes all items so they don't duplicate on click
+  while (cartContainer.firstChild) {
+    cartContainer.removeChild(cartContainer.firstChild);
+  }
+  // This adds all items from the cart to the DOM
+  cart.forEach( function(item) {
+    var para = document.createElement("p");
+    var node = document.createTextNode(item);
+    para.appendChild(node);
+    cartContainer.appendChild(para);
+  })
+})
